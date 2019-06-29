7a0262d4329ce687ff1e01516bfe56dcba7184a4
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2019-06-28 20:27:05 -0700

    Initial commit
    Signed-off-by: Wenda Ni, Ph.D <wonda.ni@gmail.com>

---
+++ hashtable.c
@@ -0,0 +1,46 @@
+struct node {
+	char *s;
+	struct node *next;
+};
+
+#define	MULTI	31
+#define NBIN	29989
+
+unsigned int hash(char *s)
+{
+	unsigned int h = 0;
+
+	while (*s) {
+		h = MULTI * h + *s;
+		s++;
+	}
+
+	return h;
+}
+
+void insert(struct node **a, char *s)
+{
+	if (!s) {
+		return;
+	}
+
+	int i = hash(s) / NBIN;
+
+	struct node *curr = a[i];
+	while (curr) {
+		if (!strcmp(curr->s, s)) {
+			return;
+		}
+		curr = curr->next;
+	}
+
+	// reach here when we do not
+	// have string s in the hash table
+	curr = (struct node *)malloc(sizeof(struct node));
+	curr->s = (char *)malloc(strlen(s) + 1);
+	strcpy(curr->s, s);
+
+	// insert
+	curr->next = a[i];
+	a[i] = curr;
+}
